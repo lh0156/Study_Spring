adb4401a6393e4e334e1329ccdff304b9771b902
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-04-22 18:39:47 +0900

    Update HelloController.java

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,9 +16,36 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("Hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model) {
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
     }
-}
+
+    @GetMapping("hello-string")
+    //body뷰에 이 내용을 직접 넣어주겠다!
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // Spring을 넣으면 hello Spring으로 바뀌겠지?
+    }
+    //얘는 큰 의미없다
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
