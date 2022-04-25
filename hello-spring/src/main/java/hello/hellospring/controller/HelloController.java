49c813402999915b1e0bc43431ab0927a9537d83
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-04-25 19:50:47 +0900

    add

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,49 +3,13 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    // 웹 어플리케이션에서 hello라고 해주면 이 어플리케이션을 호출해준다.
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello!");
+        model.addAttribute("data", "TestTest!!");
         return "hello";
     }
-
-    @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello-template";
-    }
-
-    @GetMapping("hello-string")
-    //body뷰에 이 내용을 직접 넣어주겠다!
-    @ResponseBody
-    public String helloString(@RequestParam("name") String name) {
-        return "hello " + name; // Spring을 넣으면 hello Spring으로 바뀌겠지?
-    }
-    //얘는 큰 의미없다
-
-
-    @GetMapping("hello-api")
-    @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name) {
-        Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
-    }
-    static class Hello {
-        private String name;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
+}
