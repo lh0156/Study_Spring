e21cf8c23c718968e78172f3bca5ceeb8cb30c8d
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-05-07 23:53:53 +0900

    add Order Service Test Only Java Code version (use main method)

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    //이렇게 메인 메소드로 하는것보다 자동화된 Test로 하는게 제일 좋음 Junit스자!
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("Order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+    }
+
+}
