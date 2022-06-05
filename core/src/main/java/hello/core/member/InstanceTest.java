d800626ba552d1919da1f714d5b2f02d20a95b28
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-06-05 22:07:12 +0900

    Add implement Singleton Instance (Original Java Code)

---
+++ core/src/main/java/hello/core/member/InstanceTest.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+public class InstanceTest {
+    private static InstanceTest test;
+
+    public InstanceTest() {
+
+    }
+
+    void Save() {
+
+    }
+
+    void viewInstance() {
+        System.out.println(InstanceTest.test);
+
+    }
+}
