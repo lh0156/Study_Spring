d32a166939260d08d6488f9447bc604e430cdf79
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-06-06 01:51:55 +0900

    Add Component Scan Example & SingleTon

---
+++ core/src/main/java/hello/core/Component/ComponentClass.java
@@ -0,0 +1,15 @@
+package hello.core.Component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentClass {
+
+    public void save() {
+        System.out.println("세이브합니다.");
+    }
+
+    private void join() {
+        System.out.println("조인 합니다.");
+    }
+}
