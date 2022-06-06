cdebf5d3e41a0eef299f5a72804f3ae1684b46cc
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-06-06 14:00:33 +0900

    SingleTon state Version

--- core/src/test/java/hello/core/pratice/Component/ComponentTest.java
+++ core/src/test/java/hello/core/pratice/Component/ComponentTest.java
@@ -1,16 +1,23 @@
-package hello.core.Component;
+package hello.core.pratice.Component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ComponentTest {
 
+    @Autowired
+    private Autowired autoInstance;
+
     public static void main(String[] args) {
+
+
         ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentConfig.class);
 
         ComponentClass bean = ac.getBean(ComponentClass.class);
 
         bean.save();
+        bean.join();
     }
 
 }
