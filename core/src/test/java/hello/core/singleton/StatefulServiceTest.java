98ef2b6ef18596180f7a1818bf8b0c06f46b0d46
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-06-07 00:02:04 +0900

    Add Singleton Stateful Code

--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -16,22 +16,21 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA : A사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int price = statefulService1.order("userA", 10000);
         //ThreadB : B사용자 20000원 주문
-        statefulService2.order("userA", 20000);
+        int price2 = statefulService2.order("userA", 20000);
 
         //ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
+
         System.out.println("price = " + price);
-        //20000
-        int price2 = statefulService2.getPrice();
+
         System.out.println("price2 = " + price2);
-        //20000
+
 
         //왜 pricesms 20000원이 나오냐?;;;
         //instance를 같은 애를 써서 이런 문제가 나옴
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        Assertions.assertThat(price).isNotEqualTo(20000);
     }
 
     static class TestConfig {
