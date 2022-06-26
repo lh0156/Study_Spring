c280abef712fe38754d95f2dad2145090299ce3c
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-06-26 13:43:17 +0900

    Add Conponent Scan Preview

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -4,9 +4,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+/*
+    매커니즘
+    1. AutoAppConfig에 ComponentScan이 있네?
+    2. Component로 되어있는거 다 뒤져
+    3. 근데 Configuration에도 Component가 있음;; 그래서 excludeFilter로 걸러준 것
+    4. 만약 의존관계가 있을 시에는 의존관계를 Autowired로 주입한다.
+
+*/
+
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
+
 }
