97fcfb9f9ab56cdb772b922f516fb18c926f904d
Eom Yunseop <lh0156@naver.com>
2022-07-29 17:42:19 +0900

    Add Constructor Injection

--- core/src/test/java/hello/core/order/OrderServiceImplTest.java
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -14,12 +14,11 @@ class OrderServiceImplTest {
 
     @Test
     void createOrder() {
-
-        MemberRepository memberRepository = new MemoryMemberRepository();
-        memberRepository.save(new Member(1L, "name", Grade.VIP));
-
-        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy();
+        MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+        memoryMemberRepository.save(new Member(1L, "윤섭", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(memoryMemberRepository, new FixDiscountPolicy());
         Order order = orderService.createOrder(1L, "itemA", 10000);
-        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
     }
 }
