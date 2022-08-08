127d425d50212b5e4e10da9476f2f36525aae0dc
Eom Yunseop <lh0156@naver.com>
2022-08-08 11:46:50 +0900

    Add Web Libraries

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -36,53 +37,28 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        System.out.println("c ount1 = " + count1);
+        System.out.println("count1 = " + count1);
         System.out.println("count2 = " + count2);
         Assertions.assertThat(count2).isEqualTo(2);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private PrototypeBean prototypeBean; //생성 시점에 주입 x01
 
         @Autowired
-        ApplicationContext applicationContext;
+        ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
-
-            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
-
-    }
-
-    @Scope("singleton")
-    static class ClientBean2 {
-        private PrototypeBean prototypeBean; //생성 시점에 주입 x02
-
-        @Autowired
-        ApplicationContext applicationContext;
-
-        @Autowired
-        public ClientBean2(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic() {
-
-            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
-            prototypeBean.addCount();
-            int count = prototypeBean.getCount();
-            return count;
-        }
-
     }
 
     @Scope("prototype")
