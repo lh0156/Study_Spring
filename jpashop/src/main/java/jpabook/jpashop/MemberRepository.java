d84e74ce476112e6cfe037cd3b90eb5819dfc120
Eom Yunseop <lh0156@naver.com>
2022-08-10 20:12:09 +0900

    Add Example

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+//Componenet Scan의 대상이 되는 Annotation 중 하나
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,8 @@
+package jpabook.jpashop;
+
+import static org.junit.Assert.*;
+
+
+public class MemberRepositoryTest {
+
+}
