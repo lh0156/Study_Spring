50e93436c108f7602f53e9cc622314e220be5dd6
Eom Yunseop <lh0156@naver.com>
2022-08-18 16:06:46 +0900

    하..

--- hello-spring2/src/main/java/hello/hellospring2/repository/MemoryMemberRepository.java
+++ hello-spring2/src/main/java/hello/hellospring2/repository/MemoryMemberRepository.java
@@ -4,13 +4,11 @@ import hello.hellospring2.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
- */
 @Repository
 public class MemoryMemberRepository implements MemberRepository {
-    private static Map<Long, Member> store = new HashMap<>();
+    private static ConcurrentHashMap<Long, Member> store = new ConcurrentHashMap<>();
     private static long sequence = 0L;
 
     @Override
