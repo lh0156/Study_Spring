7357cada3e8f763b0db8d819465e38572db64176
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-08-24 13:49:05 +0900

    use Static nested Class

--- spring-test/day2/java-config/src/main/java/com/oraclejava/day2/JavaConfigDemo.java
+++ spring-test/day2/java-config/src/main/java/com/oraclejava/day2/JavaConfigDemo.java
@@ -1,5 +1,31 @@
 package com.oraclejava.day2;
 
+import com.oraclejava.day1.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 public class JavaConfigDemo {
+
     //정적 중첩 클래스
+    @Configuration
+    static class AppConfig {
+
+        @Bean
+        public Person person() {
+            Person person = new Person();
+            person.setName("현주엽");
+            person.setAge(47);
+            return person;
+        }
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        Person person = ac.getBean("person", Person.class);
+        System.out.println(person);
+    }
+
+    
 }
