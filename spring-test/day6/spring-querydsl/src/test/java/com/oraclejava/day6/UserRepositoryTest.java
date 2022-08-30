1338961708a68d60354e9ffede746f7d5b5aa759
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-08-30 13:57:27 +0900

    Update UserRepositoryTest Class

--- spring-test/day6/spring-querydsl/src/test/java/com/oraclejava/day6/UserRepositoryTest.java
+++ spring-test/day6/spring-querydsl/src/test/java/com/oraclejava/day6/UserRepositoryTest.java
@@ -1,14 +1,15 @@
 package com.oraclejava.day6;
 
 import com.oraclejava.day6.entity.BlogPost;
+import com.oraclejava.day6.entity.QUser;
 import com.oraclejava.day6.entity.User;
 import com.oraclejava.day6.repository.UserRepository;
+import com.querydsl.core.types.Predicate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @SpringBootTest
@@ -19,28 +20,31 @@ public class UserRepositoryTest {
 
     @Test
     public void testSave() {
-        User user = new User();
-        user.setDisabled(false);
-        user.setLogin("hyunjung");
+//        User user = new User();
+//        user.setDisabled(false);
+//        user.setLogin("yunseop");
 //      윤섭(1) 이 글을 쓴다
 
-//        User user = dao.findById(5L).get();
-//
-//        BlogPost blogPost = new BlogPost();
-//        blogPost.setTitle("he is genius?");
-//        blogPost.setBody("He is perfect boy!");
-//        blogPost.setUser(user);
-//
-//        Set<BlogPost> blogPostSet = new HashSet<>();
-//        blogPostSet.add(blogPost);
-//
-//        user.setBlogPosts(blogPostSet);
+        User user = dao.findById(13L).get();
+
+        BlogPost blogPost = new BlogPost();
+        blogPost.setTitle("hanesome");
+        blogPost.setBody("chanchan hanesome he is my friend");
+        blogPost.setUser(user);
+
+        Set<BlogPost> blogPostSet = new HashSet<>();
+        blogPostSet.add(blogPost);
+
+        user.setBlogPosts(blogPostSet);
 
         dao.save(user);
     }
 
     @Test
     public void testFindUserByUserName() {
-        
+        QUser quser = QUser.user;
+        Predicate predicate = quser.login.eq("changyu");
+        User user = dao.findOne(predicate).get();
+        //System.out.println(user.getLogin());
     }
 }
