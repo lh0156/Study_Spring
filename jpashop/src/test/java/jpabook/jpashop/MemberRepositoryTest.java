21cf0c9d38959afe4ebdf33ae04aefa8e0ace375
Eom Yunseop <lh0156@naver.com>
2022-08-31 11:09:20 +0900

    jpa dependency Code

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,40 @@
 package jpabook.jpashop;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootApplication
 class MemberRepositoryTest {
 
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        System.out.println(saveId);
+
+
+    }
 }
