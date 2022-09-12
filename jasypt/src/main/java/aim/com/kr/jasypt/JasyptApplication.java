2b0229ac8e6f8a000869e1dd7b78ed7ac8e996cd
Eom Yunseop <lh0156@naver.com>
2022-09-13 08:47:44 +0900

    Update JasyptApplication.java

--- jasypt/src/main/java/aim/com/kr/jasypt/JasyptApplication.java
+++ jasypt/src/main/java/aim/com/kr/jasypt/JasyptApplication.java
@@ -1,21 +1,23 @@
 package aim.com.kr.jasypt;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
-@Slf4j
 @SpringBootApplication
 public class JasyptApplication {
 
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(JasyptApplication.class, args);
-//		ApplicationContext ac = new AnnotationConfigApplicationContext(EncryptUtil.class);
-//		EncryptUtil bean = ac.getBean(EncryptUtil.class);
-//		bean.Encryptor("테스트");
+
+		ApplicationContext ac = new AnnotationConfigApplicationContext(EncryptUtil.class);
+		EncryptUtil bean = ac.getBean(EncryptUtil.class);
+
+		bean.Encryptor("테스트");
+
+
 	}
 
 }
