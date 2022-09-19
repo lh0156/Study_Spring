3ded26b1e1cad565b91324f16a6faf4299c085e7
Eom Yunseop <lh0156@naver.com>
2022-09-19 15:45:14 +0900

    Add Web-MVC Defalut Project

---
+++ hello-MVC/src/test/java/hello/helloMVC/HelloMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.helloMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
