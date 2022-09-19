8ef72bd43ca4c7b3c9eae4d4d3e8e5c65a4414f9
Eom Yunseop <lh0156@naver.com>
2022-09-19 15:42:45 +0900

    add Async Library Import

--- scheduler/src/main/java/test/spring/scheduler/ScheduleTest.java
+++ scheduler/src/main/java/test/spring/scheduler/ScheduleTest.java
@@ -1,5 +1,6 @@
 package test.spring.scheduler;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
