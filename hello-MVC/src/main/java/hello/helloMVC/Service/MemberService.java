077fa2c319813440548a939641e5b3f5916bcdd1
Eom Yunseop <lh0156@naver.com>
2022-09-21 16:53:11 +0900

    Update MemberService.java

--- hello-MVC/src/main/java/hello/helloMVC/Service/MemberService.java
+++ hello-MVC/src/main/java/hello/helloMVC/Service/MemberService.java
@@ -4,9 +4,16 @@ import hello.helloMVC.domain.Member;
 import hello.helloMVC.repository.MemberRepository;
 import hello.helloMVC.repository.MemoryMemberRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /*
     *  회원가입
@@ -14,18 +21,31 @@ public class MemberService {
 
     public Long join(Member member) {
 
-
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
 
     }
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-                   throw new Illg
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
 
     }
 
+    /*
+    * 전체 회원 조회
+    * */
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 
 
 }
