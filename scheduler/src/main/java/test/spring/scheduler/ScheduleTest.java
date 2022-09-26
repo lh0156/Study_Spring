a1cb16986862fdce0abca36103d125c3e27d9061
Eom Yunseop <lh0156@naver.com>
2022-09-26 16:00:37 +0900

    Update 2022.09.26.16:00

---
+++ scheduler/src/main/java/test/spring/scheduler/ScheduleTest.java
@@ -0,0 +1,68 @@
+package test.spring.scheduler;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.UUID;
+
+@Slf4j
+@Component // 추가
+@EnableAsync // 추가
+public class ScheduleTest {
+
+    @Scheduled(cron = "0 0 7 1/1 * *")
+    public void scheduleTaskUsingCronExpression() {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String temp = localDateTime.toString();
+        String semiDate = ((temp.replaceAll("-", "")).replaceAll(":", ""));
+        String date = semiDate.substring(0, 8);
+        System.out.println(date);
+
+        Random random = new Random();
+        String uuid = UUID.randomUUID().toString();
+
+        String code = "cd C:\\Users\\aim\\Desktop\\Study-AimTraining\\commit && echo print('hello world') > "
+                + date
+                +"GoodDay.txt && git add *&& git commit -m \""
+                + date
+                +"\"&&git push origin main";
+
+        execCmd(code);
+
+    }
+
+    public static String execCmd(String cmd) {
+        try {
+            Process process = Runtime.getRuntime().exec("cmd /c " + cmd);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+
+            String line = null;
+            StringBuffer sb = new StringBuffer();
+            sb.append(cmd);
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+
+            String s = sb.toString();
+            String s2 = new String(s.getBytes("utf-8"));
+
+            return s2;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
--- scheduler/src/main/java/test/spring/scheduler/SchedulerApplication.java
+++ scheduler/src/main/java/test/spring/scheduler/SchedulerApplication.java
@@ -1,17 +1,13 @@
 package test.spring.scheduler;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling // 추가
 @SpringBootApplication
-@Slf4j
 public class SchedulerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerApplication.class, args);
 	}
-
-
 }
