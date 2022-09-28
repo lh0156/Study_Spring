872ef4968c909f42439e2a81e5f5d7aa7c966bde
Eom Yunseop <lh0156@naver.com>
2022-09-29 08:23:47 +0900

    Update SchedulerApplication.java

--- scheduler/src/main/java/test/spring/scheduler/SchedulerApplication.java
+++ scheduler/src/main/java/test/spring/scheduler/SchedulerApplication.java
@@ -1,5 +1,6 @@
 package test.spring.scheduler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling // 추가
 @SpringBootApplication
 public class SchedulerApplication {
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerApplication.class, args);
 	}
