663a8331bb947fbc1e6a6ee1918eae22c9a105bc
Eom Yunseop <lh0156@naver.com>
2022-10-17 16:52:51 +0900

    Update Encrypt Project

---
+++ Encrypt Project/src/main/java/encrypt/demo/domain/Value.java
@@ -0,0 +1,31 @@
+package encrypt.demo.domain;
+
+public class Value {
+    String in;
+    String pc;
+    String agr;
+
+    public String getIn() {
+        return in;
+    }
+
+    public void setIn(String in) {
+        this.in = in;
+    }
+
+    public String getPc() {
+        return pc;
+    }
+
+    public void setPc(String pc) {
+        this.pc = pc;
+    }
+
+    public String getAgr() {
+        return agr;
+    }
+
+    public void setAgr(String agr) {
+        this.agr = agr;
+    }
+}
