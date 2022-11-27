a88770056fda5cab341fdd99f31b37113895aabf
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-11-27 20:05:16 +0900

    Update Servlet Project

--- springmvc/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ springmvc/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 서블릿 자동 등록스프링에서 서블릿을 찾아서 등록하는 것
 @SpringBootApplication
 public class ServletApplication {
 
