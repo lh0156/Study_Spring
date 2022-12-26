e0002c44fbea5aadb2d0f7212fccacdab6a0af55
Eom Yunseop <lh0156@naver.com>
2022-12-26 12:42:44 +0900

    update thymleaf function

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -56,6 +57,36 @@ public class BasicController {
         model.addAttribute("servletContext", request.getServletContext());
         return "basic/basic-objects";
     }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Sprong!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+
+        return "basic/operation";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
