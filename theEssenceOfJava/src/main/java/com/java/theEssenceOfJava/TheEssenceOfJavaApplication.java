cabe3af10993d39406c2a735df8ec589e0319570
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-01-08 18:11:47 +0900

    init 자바의 정석 Project

---
+++ theEssenceOfJava/src/main/java/com/java/theEssenceOfJava/TheEssenceOfJavaApplication.java
@@ -0,0 +1,13 @@
+package com.java.theEssenceOfJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheEssenceOfJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheEssenceOfJavaApplication.class, args);
+	}
+
+}
---
+++ theEssenceOfJava/src/test/java/com/java/theEssenceOfJava/TheEssenceOfJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.theEssenceOfJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheEssenceOfJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
