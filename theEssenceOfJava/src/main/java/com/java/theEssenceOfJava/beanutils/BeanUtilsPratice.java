10989774616b8ffc2ec7121b583e875fe88bee2e
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-01-22 08:31:42 +0900

    add BeanIUtils copyTo Method
    this method is copy from source to target property

---
+++ theEssenceOfJava/src/main/java/com/java/theEssenceOfJava/beanutils/BeanUtilsPratice.java
@@ -0,0 +1,59 @@
+package com.java.theEssenceOfJava.beanutils;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+
+public class BeanUtilsPratice {
+
+    public static void main(String[] args) {
+
+        Member member = new Member();
+        member.setName("윤섭");
+        member.setAddress("신림");
+        member.setAge(27);
+
+        MemberDto memberDto = new MemberDto();
+
+        MemberDto resultMemberDto = copyTo(member, memberDto);
+
+        System.out.println(resultMemberDto.getAge());
+        System.out.println(resultMemberDto.getName());
+
+    }
+
+
+
+    public static <S, T> T copyTo (S source, T target) {
+
+        if (source == null) {
+            return null;
+        }
+
+        if (target == null) {
+            return null;
+        }
+
+        BeanUtils.copyProperties(source, target);
+
+        return target;
+
+    }
+
+    @Getter
+    @Setter
+    static class MemberDto {
+        String name;
+        int age;
+    }
+
+    @Getter
+    @Setter
+    static class Member {
+        String name;
+        String address;
+        int age;
+    }
+
+}
