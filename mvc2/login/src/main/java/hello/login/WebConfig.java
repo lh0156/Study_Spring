5b37587e32305e9638de403eb200ce003be4fa3e
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-09-28 23:24:16 +0900

    feat: servlet filter

---
+++ mvc2/login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,24 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+import java.util.Collections;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.setUrlPatterns(Collections.singleton("/*"));
+
+        return filterFilterRegistrationBean;
+
+    }
+}
