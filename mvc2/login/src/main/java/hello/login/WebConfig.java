b7079752a238059ed827cf26dd8114497fa4536d
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-10-02 11:52:31 +0900

    feat: Spring Intercepter Log

--- mvc2/login/src/main/java/hello/login/WebConfig.java
+++ mvc2/login/src/main/java/hello/login/WebConfig.java
@@ -1,21 +1,44 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import java.util.Collections;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
 
     @Bean
     public FilterRegistrationBean<Filter> logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
         filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.setUrlPatterns(Collections.singleton("/*"));
+
+        return filterFilterRegistrationBean;
+
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> logCheckFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterFilterRegistrationBean.setOrder(2);
         filterFilterRegistrationBean.setUrlPatterns(Collections.singleton("/*"));
 
         return filterFilterRegistrationBean;
