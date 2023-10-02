48758cc0379b0b9fa61fae34f0f51413bfeda0b0
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-10-02 20:38:18 +0900

    feat: Spring Intercepter LogIn Check

--- mvc2/login/src/main/java/hello/login/WebConfig.java
+++ mvc2/login/src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,9 +22,16 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/home", "/member/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
+
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean<Filter> logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -34,7 +42,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean<Filter> logCheckFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
